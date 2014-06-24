93e59ab31395821639dfcc83dcd9f4255afd69b7
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-06-24 15:47:45 +0100

    Added an icon and added a directory for the admin account on Windows
    |INSTALLER|
    * Added an icon (it's not clear whether this is for the Alt+Tab interface, as
      it has not been tried on a DE with such abilities)
    * Added a directory for the administrator account on Windows to install
    * (should be <WINDRIVE>:/Program Files on 32-bit native and 64-bit, and
      <WINDRIVE>:/Program Files (x86) on WoW64
    * Explicitly stated the Start Menu sub directory

--- createinstaller.sh
+++ createinstaller.sh
@@ -5,7 +5,7 @@ mkdir -p $DOWNLOADDIR
 
 if [[ ${HOSTMACH} == "i686-w64-mingw32" ]]; then
 	EXTENSION=exe
-	ADMINDIR=?
+	ADMINDIR="@RootDir@"
 	INSTALLERBASE=$DOWNLOADDIR/qtifw-win-x86/ifw-bld/bin/installerbase.exe
 	cd $DOWNLOADDIR
 	wget -c http://download.qt-project.org/snapshots/ifw/1.5/2014-02-13_50/installer-framework-build-win-x86.7z
@@ -13,7 +13,7 @@ if [[ ${HOSTMACH} == "i686-w64-mingw32" ]]; then
 	cd $ROOTDIR
 elif [[ ${HOSTMACH} == "x86_64-w64-mingw32" ]]; then
 	EXTENSION=exe
-	ADMINDIR=?
+	ADMINDIR="@RootDir@"
 	INSTALLERBASE=$DOWNLOADDIR/qtifw-win-x64/ifw-bld/bin/installerbase.exe
 	cd $DOWNLOADDIR
 	wget -c http://download.qt-project.org/snapshots/ifw/1.5/2014-02-13_50/installer-framework-build-win-x86.7z
@@ -79,6 +79,8 @@ cat > installer/config/config.xml << __EOF__
 	<TargetDir>@homeDir@/saturndev/saturn-sdk</TargetDir>
 	<AdminTargetDir>${ADMINDIR}</AdminTargetDir>
 	<Watermark>watermark.png</Watermark>
+	<InstallerWindowIcon>icon.png</InstallerWindowIcon>
+	<StartMenuDir>SEGA Saturn SDK</StartMenuDir>
 
 	<RemoteRepositories>
 		<Repository>
